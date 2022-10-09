a6b67e497c169a4fef4c1a5c74a806351e565d50
Gershon <gershon.qiu@gmail.com>
2022-10-09 23:20:29 +0800

    Update auto_update.sh

--- auto_update.sh
+++ auto_update.sh
@@ -15,5 +15,5 @@ echo 'get subscribe'
 curl -O $urlpath
 mv $currentdate.yaml  $filename
 echo 'modify select mode'
-sed -i 's#type: select#type: url-test\n    url: http://www.gstatic.com/generate_204\n    tolerance: 50\n    interval: 300#g' $filename
+sed -i '1,/聊天软件/s#type: select#type: url-test\n url: http://www.gstatic.com/generate_204\n tolerance: 50\n interval: 300#g' $filename
 echo 'bye~'
